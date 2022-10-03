8a7cea386dcbb655dae1064f07c79a962dc6042c
Luis Fabian <fabianciittoo@gmail.com>
2022-10-02 21:49:28 -0400

    Agregar archivos de proyecto.

---
+++ HolaMundo/principal.cpp
@@ -0,0 +1,8 @@
+//
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "Hola Mundo " << endl;
+	cout << "Luis Fabian Jorge Cedillo" << endl;
+	cout << "CI: 10526518 CU:35-5276" << endl;
+}
